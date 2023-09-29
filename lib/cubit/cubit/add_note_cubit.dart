4fb9fb961956bf3f489b983dfc6f7f7656fced3d
Your Name <you@example.com>
2023-09-29 10:49:18 +0300

    add status to add note cubit

--- lib/cubit/cubit/add_note_cubit.dart
+++ lib/cubit/cubit/add_note_cubit.dart
@@ -1,10 +1,22 @@
 import 'package:bloc/bloc.dart';
+import 'package:hive/hive.dart';
 import 'package:meta/meta.dart';
+import 'package:note_app2/models/note_model.dart';
+import 'package:note_app2/widgets/constants.dart';
 
 part 'add_note_state.dart';
 
 class AddNoteCubit extends Cubit<AddNoteState> {
   AddNoteCubit() : super(AddNoteInitial());
-
-  
+   
+ 
+  addNote(NoteModel note)async{
+  emit(AddNoteLoading());
+  try {
+  await Hive.box(kNoteBox).add(note);
+  emit(AddNoteSuccess());
+} catch (e) {
+  emit(AddNoteFailure(e.toString()));
+}
+  }
 }
