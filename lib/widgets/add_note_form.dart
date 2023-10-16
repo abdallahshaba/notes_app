062cc53a633a12182fe6d84d40d97ccb38765c5c
Your Name <you@example.com>
2023-10-16 17:41:31 +0300

    create model note

--- lib/widgets/add_note_form.dart
+++ lib/widgets/add_note_form.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:note_app2/cubit/cubit/add_note_cubit.dart';
+import 'package:note_app2/models/note_model.dart';
 
 import 'custom_button.dart';
 import 'custom_textfield.dart';
@@ -13,6 +16,7 @@ class AddNoteForm extends StatefulWidget {
 }
 
 class _AddNoteFormState extends State<AddNoteForm> {
+
   String? caption , content ;
  final GlobalKey<FormState> formKey = GlobalKey();
  AutovalidateMode autovalidateMode = AutovalidateMode.disabled ;
@@ -47,9 +51,10 @@ class _AddNoteFormState extends State<AddNoteForm> {
             onTap: () {
               if(formKey.currentState!.validate()){
                 formKey.currentState!.save();
-              }
+           }  
               else{
                 autovalidateMode = AutovalidateMode.always ;
+                setState(() {});
               }
             },
            ),
